79fc9ed3be93d567126616ced551b64fd27a94ab
Franczyj <jakefranczyk@yahoo.com>
2022-04-15 10:08:42 -0500

    Revert "second commit"
    This reverts commit e5e95be0

--- main.py
+++ main.py
@@ -4,4 +4,28 @@ def pullData(api, credentials):
     #hub.update_dbs(credentials, api, tckrs='', modeling=True, forceFDataPull=False, forceMDataPull=False, verbose=True)
     hub.update_dbs(credentials, api, tckrs=['TSLA', 'AAPL', 'MSFT','TWTR'], modeling=True, forceFDataPull=False, forceMDataPull=False, verbose=False)
 
-print('hello')
+
+def run_stock_analysis():
+    loginSuccessful = False
+    api=''
+    credentials=''
+    loginSuccessful, api, credentials = hub.init()
+    # print(loginSuccessful,api,credentials)
+
+    if loginSuccessful:
+        # print(api.get_account())
+        pullData(api, credentials)
+
+        # dataset = hub.get_table(dataset='DAILY MARKET DATA', raw=True)
+    keys = hub.get_datasets()
+    print(keys)
+    dataset = hub.get_table(dataset=hub.get_datasets(), raw=False)
+    # print(dataset['marketData'])
+    dailyDf = dataset['marketData']['DAILY MARKET DATA']['STAT DATA']
+    print(dailyDf.head(10).to_string())
+    # print(dataset['DAILY MARKET DATA']['RAW DATA'].to_string())
+    print(dailyDf.describe(include="all").to_string())
+    print(dailyDf.info())
+
+if __name__ == '__main__':
+    run_stock_analysis()
